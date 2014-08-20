94e5b13966dc3bbc2f35730cb09d0f741e97d0ff
shruti sharma <shrutijalewar@gmail.com>
2014-08-20 13:32:34 -0500

    models and test

---
+++ app/models/treasure.js
@@ -0,0 +1,36 @@
+'use strict';
+var Mongo = require('mongodb'),
+    _     = require('lodash');
+
+function Treasure(o){
+  this.name = o.name;
+  this.loc = {name:o.name, lat: parseFloat(o.lat), lng: parseFloat(o.lng)};
+  this.photo = o.photo;
+  this.difficulty = o.difficulty;
+  this.hint = o.hint;
+}
+
+Object.defineProperty(Treasure, 'collection', {
+  get: function(){return global.mongodb.collection('treasures');}
+});
+
+Treasure.all = function(cb){
+  Treasure.collection.find().toArray(cb);
+};
+
+Treasure.create = function(o, cb){
+  var t = new Treasure (o);
+  Treasure.collection.save(t, cb);
+};
+Treasure.findById= function(id, cb){
+  var _id = Mongo.ObjectID(id);
+  Treasure.collection.findOne({_id:_id},function(err, obj){
+    var treasure = changePrototype(obj);
+    cb(treasure);
+  });
+};
+module.exports = Treasure;
+
+function changePrototype(obj){
+  return _.create(Treasure.prototype, obj);
+}
---
+++ test/unit/treasure.js
@@ -0,0 +1,64 @@
+/* jshint expr:true */
+/* global describe, it, before, beforeEach */
+
+'use strict';
+
+var expect    = require('chai').expect,
+    Treasure    = require('../../app/models/treasure'),
+    dbConnect = require('../../app/lib/mongodb'),
+    cp        = require('child_process'),
+    Mongo     = require('mongodb'),
+    db        = 'treasure-map';
+
+describe('Treasure', function(){
+  before(function(done){
+    dbConnect(db, function(){
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      done();
+    });
+  });
+
+  describe('constructor', function(){
+    it('should create a new Treasure object', function(){
+      var f = {name:'food', loc:{name:'kitchen', lat:'36', lng:'-86.7'}, photo:'yum.com', hint:'open fridge. Eat'},
+          t = new Treasure(f);
+      expect(t).to.be.instanceof(Treasure);
+    });
+  });
+
+  describe('.all', function(){
+    it('should get all treasures', function(done){
+      Treasure.all(function(err, treasures){
+        expect(treasures).to.have.length(2);
+        done();
+      });
+    });
+  });
+});
+
+describe('.create', function(){
+  it('should create a new treasure in the database', function(done){
+    var f = {name:'food', loc:{name:'kitchen', lat:'36', lng:'-86.7'}, photo:'yum.com', difficulty:'easy', hint:'open fridge. Eat'};
+    Treasure.create(f, function(err, treasure){
+      expect(treasure._id). to.be.instanceof(Mongo.ObjectID);
+      done();
+    });
+  });
+});
+
+describe('.findById', function(){
+  it('should find one treasure object out of the database', function(done){
+    Treasure.findById('000000000000000000000001',function(treasure){
+      //console.log(treasure);
+      expect(treasure.name).to.equal('Gold');
+      expect(treasure.loc.name).to.equal('paris');
+      expect(treasure.difficulty).to.equal('hard');
+      done();
+    });
+  });
+});
